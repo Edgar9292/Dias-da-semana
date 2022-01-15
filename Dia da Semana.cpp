320fd4823b63696585c6d452e25b3a7834e6a0d2
Edgar9292 <92225390+Edgar9292@users.noreply.github.com>
2022-01-15 17:14:33 -0300

    Add files via upload

---
+++ Dia da Semana.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int day;
+
+    cout << "Dia da semana: ";
+    cin >> day;
+
+    switch(day)
+    {
+        case 1:
+            cout <<"Domingo \n";
+            break;
+        case 2:
+            cout <<"Segunda \n";
+            break;
+        case 3:
+            cout <<"Terça \n";
+            break;
+        case 4:
+            cout <<"Quarta \n";
+            break;
+        case 5:
+            cout <<"Quinta \n";
+            break;
+        case 6:
+            cout <<"Sexta \n";
+            break;
+        case 7:
+            cout <<"Sábado \n";
+            break;
+        default:
+            cout <<"Dia inválido";
+    }
+
+    return 0;
+}
